120cc52f741473836b8f147347046f378212d76a
Diego Castillo <diego.castilloa@upb.edu.co>
2021-09-23 23:16:02 -0500

    Refactoring contador

--- Parcial2Scripting/Assets/Scripts/Contador.cs
+++ Parcial2Scripting/Assets/Scripts/Contador.cs
@@ -3,47 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.UI;
-public class Contador : MonoBehaviour
-{
-    private int contador = 0;
-    public int FinJuego = 4;
-    public delegate void OnButtonClick();
-    public event OnButtonClick Cambio;
-    public Button Fin;
-    public Button Inicio;
-    public GameObject PantallaFin;
-   
 
-   
-    public void Awake()
+[CreateAssetMenu(fileName = "Contador")]
+public class Contador : ScriptableObject
+{
+    public int contador = 0;
+    public Canal canal;
+
+    void Awake()
     {
-        //Jugador.gameObject.SetActive(true);
-        Inicio.gameObject.SetActive(true);
-       Fin.gameObject.SetActive(false);
-        PantallaFin.gameObject.SetActive(false);
+        contador = 0;
+        canal.Cambiado += ContadorBoton;
     }
-    public void ButtonClick()
+    public void ContadorBoton()
     {
         contador++;
-       Cambio?.Invoke();
         Debug.Log("Puntos: " + contador);
 
-        if(contador == FinJuego)
-        {
-
-            Fin.gameObject.SetActive(true);
-            Inicio.gameObject.SetActive(false);
-            PantallaFin.gameObject.SetActive(true);
-           
-
-        }
-
     }
+    
+   
+}
     
 
 
 
 
 
-}
+
 
